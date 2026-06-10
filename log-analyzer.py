3d62636549480037700111129713e685566be2ab
alvinewamalwa <wamalwaalvine@gmail.com>
2026-06-10 18:47:06 +0300

    Refactor comments in log-analyzer.py
    Updated comments for clarity and consistency.

--- log-analyzer.py
+++ log-analyzer.py
@@ -1,10 +1,10 @@
-# simple_log_analyzer.py
-
-A simple Python script that reads log files and identifies basic security-related events such as errors, failed login attempts, and warnings using pattern matching.
-It helps demonstrate basic log analysis and threat detection concepts used in cybersecurity.
 
 
-# Open and read the log file
+#A simple Python script that reads log files and identifies basic security-related events such as errors, failed login attempts, and warnings using pattern matching.
+#It helps demonstrate basic log analysis and threat detection concepts used in cybersecurity.
+
+
+# Opens and reads the log file
 with open("logs.txt", "r") as file:
     logs = file.readlines()
 
@@ -13,17 +13,17 @@ error_count = 0
 failed_login_count = 0
 warning_count = 0
 
-# Store suspicious lines
+# Storing suspicious lines
 suspicious = []
 
-# Loop through each line
+# Loops through each line
 for line in logs:
     line = line.strip()  # remove spaces/newlines
 
-    # Pattern matching (simple)
+    
     if "ERROR" in line:
         error_count += 1
-        suspicious.append(line)
+        suspicious.append(line)s
 
     elif "Failed login" in line:
         failed_login_count += 1
@@ -32,7 +32,7 @@ for line in logs:
     elif "WARNING" in line:
         warning_count += 1
 
-# Print summary
+
 print("=== LOG ANALYSIS SUMMARY ===\n")
 
 print(f"Errors: {error_count}")
